a1d4efb60e99cec48e7c816c75bd173857a6c71e
daniel.logerstedt@gmail.com <daniel.logerstedt@gmail.com>
2019-04-15 10:45:28 -0700

    Scaffolded File Structure

---
+++ RimrockMVC/RimrockMVC/Models/Services/FavLocationService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RimrockMVC.Models.Services
+{
+    public class FavLocationService
+    {
+    }
+}
