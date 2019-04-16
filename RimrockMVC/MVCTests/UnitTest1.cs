c87f46247c8c3468af7b343ee17558aaf8a83918
Benjamin Taylor <staylor.ben@gmail.com>
2019-04-16 13:12:50 -0700

    Added tests for FavLocationService

--- RimrockMVC/MVCTests/UnitTest1.cs
+++ RimrockMVC/MVCTests/UnitTest1.cs
@@ -5,11 +5,16 @@ using RimrockMVC.Models.Services;
 using Microsoft.EntityFrameworkCore;
 using RimrockMVC.Data;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace MVCTests
 {
     public class UnitTest1
     {
+        /*-------------------
+        | UserService Tests |
+        -------------------*/
+
         [Fact]
         public async void CanCreateUser()
         {
@@ -48,5 +53,59 @@ namespace MVCTests
                 Assert.Equal(testUser.Name, result.Name);
             }
         }
+
+        /*--------------------------
+        | FavLocationService Tests |
+        --------------------------*/
+        
+        [Fact]
+        public async void CanCreateFavLocation()
+        {
+            DbContextOptions<RimrockDBContext> options =
+                new DbContextOptionsBuilder<RimrockDBContext>()
+                .UseInMemoryDatabase("CanCreateFavLocation").Options;
+
+            using (RimrockDBContext context = new RimrockDBContext(options))
+            {
+                FavLocation location = new FavLocation() { Name = "Smith Rock" };
+                FavLocationService service = new FavLocationService(context);
+                await service.CreateFavLocation(location);
+
+                FavLocation result = context.FavLocations.Where(fl => fl.Name == location.Name).FirstOrDefault();
+
+                Assert.Equal(location.Name, result.Name);
+            }
+        }
+
+        [Fact]
+        public async void CanGetFavLocations()
+        {
+            DbContextOptions<RimrockDBContext> options =
+                new DbContextOptionsBuilder<RimrockDBContext>()
+                .UseInMemoryDatabase("CanGetFavLocations").Options;
+
+            using (RimrockDBContext context = new RimrockDBContext(options))
+            {
+                List<FavLocation> locations = new List<FavLocation>()
+                {
+                    new FavLocation() {Name = "Smith Rock"},
+                    new FavLocation() {Name = "Mount Everest"},
+                    new FavLocation() {Name = "The Arrowhead"}
+                };
+
+                await context.FavLocations.AddRangeAsync(locations);
+                await context.SaveChangesAsync();
+
+                FavLocationService service = new FavLocationService(context);
+
+                var result = await service.GetFavLocations();
+
+                Assert.Equal(3, result.Count());
+            }
+        }
+
+        /*--------------------------
+        | FavRetailerService Tests |
+        --------------------------*/
     }
 }
