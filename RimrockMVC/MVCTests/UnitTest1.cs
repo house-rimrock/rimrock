eb706c20903fc7f52366e280dc7dacfdc6bef4ea
Benjamin Taylor <staylor.ben@gmail.com>
2019-04-16 11:27:58 -0700

    Passed CanGetUser test

--- RimrockMVC/MVCTests/UnitTest1.cs
+++ RimrockMVC/MVCTests/UnitTest1.cs
@@ -28,5 +28,25 @@ namespace MVCTests
                 Assert.Equal(user.Name, dbUser.Name);
             }
         }
+
+        [Fact]
+        public async void CanGetUser()
+        {
+            DbContextOptions<RimrockDBContext> options =
+                new DbContextOptionsBuilder<RimrockDBContext>()
+                .UseInMemoryDatabase("CanGetUser").Options;
+
+            using (RimrockDBContext context = new RimrockDBContext(options))
+            {
+                User testUser = new User() { Name = "xXBloodyFangzXx" };
+                await context.AddAsync(testUser);
+                await context.SaveChangesAsync();
+                UserService service = new UserService(context);
+
+                User result = await service.GetUser(testUser.Name);
+
+                Assert.Equal(testUser.Name, result.Name);
+            }
+        }
     }
 }
