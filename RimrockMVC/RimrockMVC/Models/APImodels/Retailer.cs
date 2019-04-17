54e3513d719a8da7315c0979742482ff798928e7
Jason Burns <jasonb315@protonmail.com>
2019-04-16 17:40:17 -0700

    api models

---
+++ RimrockMVC/RimrockMVC/Models/APImodels/Retailer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rimrock_api.Models
+{
+    public class Retailer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Specialty { get; set; }
+        public int RegionID { get; set; }
+    }
+}
