04f7baef4e872f63c6e381dd0c2b5f077841dbaf
Benjamin Taylor <staylor.ben@gmail.com>
2019-04-17 12:10:46 -0700

    Can now filter search results by region

---
+++ RimrockMVC/RimrockMVC/Models/ViewModels/Search.cs
@@ -0,0 +1,14 @@
+﻿using RimrockMVC.Models.APImodels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RimrockMVC.Models.ViewModels
+{
+    public class Search<T>
+    {
+        public List<T> Results { get; set; }
+        public List<Region> Regions { get; set; }
+    }
+}
