2108ebe439d27ef89f0e0d55845e150935c5056b
Andrew Curtis <curtis.andrewm@gmail.com>
2019-04-16 19:14:29 -0700

    Add working API response-parsing method (for Region)

--- RimrockMVC/RimrockMVC/Models/APImodels/Location.cs
+++ RimrockMVC/RimrockMVC/Models/APImodels/Location.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace rimrock_api.Models
+namespace RimrockMVC.Models.APImodels
 {
     public class Location
     {
